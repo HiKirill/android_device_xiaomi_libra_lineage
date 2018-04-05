53072e9603cc3973aa3c38db27e9ae42d318d843
Владимир <vova.perm@hotmail.com>
2018-04-05 20:12:04 +0500

    libra: update fingerprint to stable V9.5.1.0

--- modules/init/init_msm.cpp
+++ modules/init/init_msm.cpp
@@ -115,15 +115,15 @@ void vendor_load_properties()
     case LIBRA_BOARD_ID:
         property_override_dual("ro.product.model", "ro.vendor.product.model", "Mi-4c");
         property_override_dual("ro.product.device", "ro.vendor.product.device", "libra");
-        property_override("ro.build.description", "libra-user 7.0 NRD90M 8.3.15 release-keys");
-        property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "Xiaomi/libra/libra:7.0/NRD90M/8.3.15:user/release-keys");
+        property_override("ro.build.description", "libra-user 7.0 NRD90M V9.5.1.0.NXKCNFA release-keys");
+        property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "Xiaomi/libra/libra:7.0/NRD90M/V9.5.1.0.NXKCNFA:user/release-keys");
         property_override_dual("ro.build.product", "ro.vendor.build.product", "libra");
         break;
     case AQUA_BOARD_ID:
         property_override_dual("ro.product.model", "ro.vendor.product.model", "Mi-4s");
         property_override_dual("ro.product.device", "ro.vendor.product.device", "aqua");
-        property_override("ro.build.description", "aqua-user 7.0 NRD90M 8.3.15 release-keys");
-        property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "Xiaomi/aqua/aqua:7.0/NRD90M/8.3.15:user/release-keys");
+        property_override("ro.build.description", "aqua-user 7.0 NRD90M V9.5.1.0.NAJCNFA release-keys");
+        property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "Xiaomi/aqua/aqua:7.0/NRD90M/V9.5.1.0.NAJCNFA:user/release-keys");
         property_override_dual("ro.build.product", "ro.vendor.build.product", "aqua");
         property_set("ro.boot.fingerprint", "fpc");
         break;
