d91bcc1495947c9c5cc21fd38ffc16907ef89a14
Владимир <vova.perm@hotmail.com>
2018-05-31 21:47:06 +0500

    libra: Change resolution fix

--- full_libra.mk
+++ full_libra.mk
@@ -14,6 +14,10 @@
 # limitations under the License.
 #
 
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1920
+TARGET_SCREEN_WIDTH := 1080
+
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
