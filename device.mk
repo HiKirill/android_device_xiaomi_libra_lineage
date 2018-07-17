0dadcc8ff7df7c3db260b9b3777c7b4b663fdcdd
Владимир <vova.perm@hotmail.com>
2018-07-18 00:50:47 +0500

    libra: move lineage overlays to overlays-lineage

--- device.mk
+++ device.mk
@@ -15,7 +15,9 @@
 #
 
 # Overlays
-DEVICE_PACKAGE_OVERLAYS := $(LOCAL_PATH)/overlay
+DEVICE_PACKAGE_OVERLAYS :=  \
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
 
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
