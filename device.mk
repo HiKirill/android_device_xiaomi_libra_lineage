ed970890180281a6bace3e68eb31f2f62733af38
Владимир <vova.perm@hotmail.com>
2018-07-18 18:12:10 +0500

    libra: dalvik-heap fix from miui 10

--- device.mk
+++ device.mk
@@ -131,11 +131,11 @@ PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.dex2oat-threads=2 \
     dalvik.vm.image-dex2oat-threads=4 \
     dalvik.vm.dex2oat-swap=false \
-    dalvik.vm.heapstartsize=16m \
-    dalvik.vm.heapgrowthlimit=192m \
+    dalvik.vm.heapstartsize=8m \
+    dalvik.vm.heapgrowthlimit=256m \
     dalvik.vm.heapsize=512m \
     dalvik.vm.heaptargetutilization=0.75 \
-    dalvik.vm.heapminfree=2m \
+    dalvik.vm.heapminfree=512k \
     dalvik.vm.heapmaxfree=8m
 
 # Display
