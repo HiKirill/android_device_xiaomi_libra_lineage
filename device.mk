5932b9922d7b525405038a5d86723c563958e960
Владимир <vova.perm@hotmail.com>
2018-07-20 12:14:30 +0500

    libra: memory fix

--- device.mk
+++ device.mk
@@ -131,11 +131,14 @@ PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.dex2oat-threads=2 \
     dalvik.vm.image-dex2oat-threads=4 \
     dalvik.vm.dex2oat-swap=false \
-    dalvik.vm.heapstartsize=8m \
+
+# Memory
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.heapstartsize=16m \
     dalvik.vm.heapgrowthlimit=256m \
     dalvik.vm.heapsize=512m \
     dalvik.vm.heaptargetutilization=0.75 \
-    dalvik.vm.heapminfree=512k \
+    dalvik.vm.heapminfree=4m \
     dalvik.vm.heapmaxfree=8m
 
 # Display
