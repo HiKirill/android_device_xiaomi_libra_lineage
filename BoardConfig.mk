33e4963ae49611188bba7110bd54e9622a3f7dc0
Владимир <vova.perm@hotmail.com>
2018-07-23 13:38:24 +0500

    libra: add qti-telephony flag

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -81,7 +81,7 @@ BOARD_USES_QCNE := true
 ifeq ($(HOST_OS),linux)
   ifneq ($(TARGET_BUILD_VARIANT),eng)
     ifeq ($(WITH_DEXPREOPT),)
-      WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := false
+      WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := true
       WITH_DEXPREOPT := true
     endif
   endif
@@ -135,6 +135,9 @@ TARGET_PROVIDES_KEYMASTER := true
 
 # Lights
 TARGET_PROVIDES_LIBLIGHT := true
+
+# QTI
+TARGET_PROVIDES_QTI_TELEPHONY_JAR := true
 
 # Lineage Hardware
 BOARD_HARDWARE_CLASS += \
