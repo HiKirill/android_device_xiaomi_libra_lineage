f8c0ac00b5f47adba1018f525da2b979d41ce21f
Владимир <vova.perm@hotmail.com>
2018-07-24 23:49:07 +0500

    libra: media profiles move to etc

--- device.mk
+++ device.mk
@@ -272,7 +272,7 @@ PRODUCT_PACKAGES += \
 
 # Media codecs
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/media_profiles_V1_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml \
+    $(LOCAL_PATH)/configs/media_profiles_V1_0.xml:system/etc/media_profiles_V1_0.xml \
     $(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
